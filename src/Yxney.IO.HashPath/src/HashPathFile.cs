be6d03bc78b011ec240739b874a15961e933de03
Geir Sørensen <geirs73@gmail.com>
2022-12-29 16:16:45 +0100

    Cleanup

--- src/Yxney.IO.HashPath/src/HashPathFile.cs
+++ src/Yxney.IO.HashPath/src/HashPathFile.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Hashing;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
--- src/Yxney.IO.HashPath/tests/DirectoryLengthsTests.cs
+++ src/Yxney.IO.HashPath/tests/DirectoryLengthsTests.cs
@@ -1,10 +1,4 @@
-using Microsoft.VisualBasic.CompilerServices;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace tests
 {
